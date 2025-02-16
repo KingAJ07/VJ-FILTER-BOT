5c1992d2a3eb3d89b572681d72ac57a9cfb00782
Aniketkmrjha <Aniketkmrjha@gmail.com>
2025-02-16 12:59:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2277,7 +2277,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "song":
         btn = [[
             InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="help"),
-            InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="telegram.me/KingVj01")
+            InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="telegram.me/Shana_Support")
         ]]
         await client.edit_message_media(
             query.message.chat.id, 
